4bed09efa1880f42873d8134b86656891299a717
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-05-04 12:35:51 +0300

    Added site4: Mountain

---
+++ site4/js/main.js
@@ -0,0 +1,53 @@
+$(function(){
+
+    $(".menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
+
+    $('.header-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        arrows: false,
+    });
+
+    $('.popular-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        prevArrow: '<img src="../images/arrows-left.jpg" alt="Prev" class="slider-arrows slider-arrows__left">',
+        nextArrow: '<img src="../images/arrows-right.jpg" alt="Next" class="slider-arrows slider-arrows__right">',
+        responsive: [
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                  slidesToShow: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+
+    $('.flag').on('click', function(){
+        $(this).toggleClass('active');
+    });
+
+    new WOW().init();
+
+});
--- site4/js/slider.js
+++
@@ -1,6 +0,0 @@
-/*jQuery(document).ready(function() { 
-	$('#button-left').on('click', function(){
-		$('header').toggleClass('slide-left');
-	});
-});
-*/
